2741767b0d4c8ba0f11ec8c3d9b0e71b7fb7ad21
Eric Swotinsky <eric.swotinsky@gmail.com>
2018-02-23 09:18:23 -0800

    initialize project structure.

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using HairSalon;
+using MySql.Data.MySqlClient;
+
+namespace HairSalon.Models
+{
+    public class Client
+    {
+        
+    }
+}
