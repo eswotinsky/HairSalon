bf473c6620c0f7e9fc073f0b03df9d04b92d0856
Eric Swotinsky <eric.swotinsky@gmail.com>
2018-03-03 18:26:55 -0800

    impleement Client.Edit() function on website.

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -40,5 +40,15 @@ namespace HairSalon.Controllers
             Client myClient = Client.Find(id);
             return View(myClient);
         }
+
+        [HttpPost("/client/update/{clientId}")]
+        public ActionResult Update(int clientId)
+        {
+            string name = Request.Form["new-client-name"];
+            int stylistId = Int32.Parse(Request.Form["new-client-stylist"]); //value
+            Client myClient = Client.Find(clientId);
+            myClient.Edit(name, stylistId);
+            return RedirectToAction("Details", new {id = clientId});
+        }
     }
 }
